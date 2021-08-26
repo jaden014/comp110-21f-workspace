34f365076b0953a4c6889c0ea6d61841d9095278
Jaden Parker <jaden014@email.unc.edu>
2021-08-26 17:31:46 -0400

    last hype machine

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -2,4 +2,4 @@
 name: str = input("What's your name, punk?")
 print(name + ", hmm, that's an interesting name.")
 print("You know what, " + name + ", I've decided I like you.")
-print("You're going to do amazingly this semester, don't let anyone doubt you, " + name + "!!!")
+print("You're going to do amazingly this semester, don't let anyone doubt you, " + name + "!!!!")
