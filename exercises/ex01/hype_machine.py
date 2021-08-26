ec2d8090ec000cc47ffed522416c0da7f887ff94
Jaden Parker <jaden014@email.unc.edu>
2021-08-26 17:25:07 -0400

    hype machine 1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,5 @@
-# TODO: Write docstring here
-
-# TODO: Initialize __author__ variable here
-
-# TODO: Implement your program logic here
+"""Jaden Parker hype machine"""
+name: str = input("What's your name, punk?")
+print(name + ", hmm, that's an interesting name.")
+print("You know what, " + name + ", I've decided I like you.")
+print("You're going to do amazingly this semester, don't let anyone doubt you, " + name + "!!!")
