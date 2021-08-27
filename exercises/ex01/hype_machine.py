c2f64feb68fa2ce85ef13478240548632723cbb5
Jaden Parker <jaden014@email.unc.edu>
2021-08-26 21:43:45 -0400

    group 1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,4 +1,5 @@
-"""Jaden Parker hype machine"""
+"""My very own, personal hype machine."""
+__author__ = "730402799"
 name: str = input("What's your name, punk?")
 print(name + ", hmm, that's an interesting name.")
 print("You know what, " + name + ", I've decided I like you.")
---
+++ exercises/ex01/numeric_operators.py
@@ -0,0 +1,8 @@
+"""Examples of numeric functions in python."""
+__author__ = "730402799"
+left_side: int = int(input("Left_integer?"))
+right_side: int = int(input("Right_integer?"))
+print(str(left_side) + " ** " + str(right_side) + " is " + str(left_side**right_side))
+print(str(left_side) + " / " + str(right_side) + " is " + str(left_side/right_side))
+print(str(left_side) + " // " + str(right_side) + " is " + str(left_side//right_side))
+print(str(left_side) + " % " + str(right_side) + " is " + str(left_side%right_side))
