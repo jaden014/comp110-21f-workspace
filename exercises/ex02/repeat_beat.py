62bbd24ac5c640d82d2bd3f28598a793f634c07a
Jaden Parker <jaden014@email.unc.edu>
2021-09-03 16:52:43 -0400

    ex02 2

--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -6,7 +6,7 @@ __author__ = "730402799"
 beat: str = input("Give me a beat!")
 amount: int = int(input("How many times should I repeat it?"))
 i: int = 1
-if amount < 0:
+if amount <= 0:
     print("No beat...")
 else:
     while i < amount:
