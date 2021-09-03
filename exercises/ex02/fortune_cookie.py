f297bfbe965c3e9de47840a27eba19be3b5b6324
Jaden Parker <jaden014@email.unc.edu>
2021-09-03 16:49:03 -0400

    ex02 1

--- exercises/ex02/fortune_cookie.py
+++ exercises/ex02/fortune_cookie.py
@@ -1,6 +1,6 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730402799"
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -12,4 +12,17 @@ __author__ = "ENTER YOUR 9-DIGIT PID HERE"
 from random import randint
 
 
-# Begin your solution here...
+print("Your fortune cookie says...")
+i: int = randint(0, 3)
+if i == 0:
+    print("It is not our abilities that determine who we really are, it is our choices.")
+else:
+    if i == 1:
+        print("HARRY DID YOU PUT YOUR NAME IN THE GOBLET OF FIRE?!")
+    else:
+        if i == 2:
+            print("Would you care for a lemon drop?")
+        else:
+            if i == 3:
+                print("To have been loved so deeply, even though the person who loved us is gone, will give us some protection forever.")
+print("Now, go spread positive vibes!")
--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -1,6 +1,14 @@
 """Repeating a beat in a loop."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730402799"
 
 
-# Begin your solution here...
+beat: str = input("Give me a beat!")
+amount: int = int(input("How many times should I repeat it?"))
+i: int = 1
+if amount < 0:
+    print("No beat...")
+else:
+    while i < amount:
+        i = i + 1
+    print((" " + str(beat)) * i)
