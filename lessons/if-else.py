a8480a3eb2d8e4ac894441594a6dd12e423a4cf2
Jaden Parker <jaden014@email.unc.edu>
2021-09-07 11:32:16 -0400

    1

---
+++ lessons/if-else.py
@@ -0,0 +1,16 @@
+"""Challenge Question #1."""
+choice: int = int(input("Enter a number: "))
+# A if choice <25
+# B if choice >=25 less than 50
+# C if over 75
+# D if >=50 less than equal 75
+if choice >= 50:
+    if choice > 75:
+        print("C")
+    else:
+        print("D")
+else:
+    if choice < 25:
+        print("A")
+    else:
+        print("B")
