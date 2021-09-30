c7483ec352075f503a57dcbe437cb83482a6a103
Jaden Parker <jaden014@email.unc.edu>
2021-09-30 17:25:10 -0400

    1

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,45 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
 
-# Define your functions below
+__author__ = "730402799"
+
+
+def only_evens(a: list[int]) -> list[int]:
+    """Returns only the even numbers in a list."""
+    i: int = 0
+    evens: list[int] = []
+    while i < len(a):
+        if a[i] % 2 == 0:
+            evens.append(a[i])
+        i += 1
+    return evens
+
+
+def sub(a: list[int], j: int, k: int) -> list[int]:
+    """Returns the piece of a list lying between 2 indices."""
+    if a == [] or j > len(a) or k <= 0:
+        return []
+    i: int = j
+    sublist: list[int] = []
+    if j < 0:
+        i = 0
+    if k > len(a):
+        k = len(a) - 1
+    while j <= i and i < k:
+        sublist.append(a[i])
+        i += 1
+    return sublist
+
+
+def concat(a: list[int], b: list[int]) -> list[int]:
+    """Returns both lists in a bigger list."""
+    i: int = 0
+    biglist: list[int] = []
+    while i < len(a):
+        biglist.append(a[i])
+        i += 1
+    i = 0
+    while i < len(b):
+        biglist.append(b[i])
+        i += 1
+    return biglist
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,78 @@
 """Unit tests for list utility functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+from exercises.ex05.utils import only_evens, sub, concat
+
+
+__author__ = "730402799"
+
+
+def test_only_evens_edge_case_empty_list() -> None:
+    """Testing an empty list."""
+    a: list[int] = []
+    assert only_evens(a) == []
+
+
+def test_only_evens_only_one_even() -> None:
+    """Testing a list with only an even number."""
+    a: list[int] = [2]
+    assert only_evens(a) == [2]
+
+
+def test_only_evens_odds_and_evens_in_list() -> None:
+    """Tests a list with many odds and evens."""
+    a: list[int] = [1, 2, 2, 4, 3]
+    assert only_evens(a) == [2, 2, 4]
+
+
+def test_sub_same_indices() -> None:
+    """Tests when indices are the same."""
+    a: list[int] = [1, 2, 3]
+    j: int = 1
+    k: int = 1
+    assert sub(a, j, k) == []
+
+
+def test_sub_empty_list() -> None:
+    """Tests empty list."""
+    a: list[int] = []
+    j: int = 1
+    k: int = 2
+    assert sub(a, j, k) == []
+
+
+def test_sub_random_case() -> None:
+    """Tests a random case."""
+    a: list[int] = [1, 2, 3, 4, 5]
+    j: int = 1
+    k: int = 4
+    assert sub(a, j, k) == [2, 3, 4]
+
+
+def test_sub_one_int() -> None:
+    """Tests a one-element-list where j < 0 and k > len(a)."""
+    a: list[int] = [1]
+    j: int = -1
+    k: int = 2
+    assert sub(a, j, k) == []
+
+
+def test_concat_empty_lists() -> None:
+    """Tests two empty lists."""
+    a: list[int] = []
+    b: list[int] = []
+    assert concat(a, b) == []
+
+
+def test_concat_nonempty_lists() -> None:
+    """Tests two nonempty lists."""
+    a: list[int] = [1, 2, 3]
+    b: list[int] = [4, 5, 6]
+    assert concat(a, b) == [1, 2, 3, 4, 5, 6]
+
+
+def test_concat_one_empty_list() -> None:
+    """Tests one empty list, one nonempty list."""
+    a: list[int] = [1, 2]
+    b: list[int] = []
+    assert concat(a, b) == [1, 2]
