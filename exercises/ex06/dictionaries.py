ed3338e62a13d3cd8bc3670017d6140cf75211be
Jaden Parker <jaden014@email.unc.edu>
2021-10-11 14:39:42 -0400

    7

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,49 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
 
-# Define your functions below
+__author__ = "730402799"
+
+
+def invert(words: dict[str, str]) -> dict[str, str]:
+    """Function to invert a dictionary."""
+    z: int = 0
+    values: list[str] = []
+    keys: list[str] = []
+    inverted_dict: dict[str, str] = {}
+    for x in words:
+        k: int = 0
+        for word in words:
+            if word == x and k != z:
+                raise KeyError("nuh-uh")
+            k += 1
+        z += 1
+        keys.append(x)
+        values.append(words[x])
+    i: int = len(keys) - 1
+    while i >= 0:
+        inverted_dict[keys[i]] = values[i]
+        i -= 1
+    return inverted_dict
+
+
+def favorite_color(names_n_colors: dict[str, str]) -> str:
+    """Function to find most repeated color."""
+    colors: dict[str, int] = {}
+    count: int = 0
+    for name in names_n_colors:
+        colors[names_n_colors[name]] = 0
+        colors[names_n_colors[name]] += 1
+    for each in colors:
+        if colors[each] > count:
+            count = colors[each]
+        if count == colors[each]:
+            return each
+
+
+def count(xs: list[str]) -> dict[str, int]:
+    counts: dict[str, int] = {}
+    for each in xs:
+        if each in counts:
+            counts[each] += 1
+        else: counts[each] = 1
+    return counts
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,57 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730402799"
+
+
+def test_invert_empty_dict() -> None:
+    """Tests for empty dictionary."""
+    words: dict[str, str] = {}
+    assert invert(words) == {}
+
+
+def test_invert_same_key() -> None:
+    """Random Test."""
+    words: dict[str, str] = {"Hello": "Hola", "One": "No", "Hell": "Bonjour"}
+    assert invert(words) == {"Hell": "Bonjour", "One": "No", "Hello": "Hola"}
+
+
+def test_invert_same_key1() -> None:
+    """Another random Test."""
+    words: dict[str, str] = {"A": "B", "B": "D", "C": "B"}
+    assert invert(words) == {"C": "B", "B": "D", "A": "B"}
+
+
+def test_favorite_color_empty_dictionary() -> None:
+    """Tests for empty dictionary."""
+    fave_colors: dict[str, str] = {}
+    assert favorite_color(fave_colors) is None
+
+
+def test_favorite_color_random_case() -> None:
+    """Tests random case."""
+    fave_colors: dict[str, str] = {"A": "Blue", "B": "Blue", "C": "Green"}
+    assert favorite_color(fave_colors) == "Blue"
+
+
+def test_favorite_color_another_random_case() -> None:
+    """Another random case."""
+    fave_colors: dict[str, str] = {"A": "Blue", "B": "Blue", "C": "Green", "D": "Green"}
+    assert favorite_color(fave_colors) == "Blue"
+
+
+def test_count_empty_list() -> None:
+    xs: list[str] = []
+    assert count(xs) == {}
+
+
+def test_count_random_case() -> None:
+    xs: list[str] = ["tre", "tre", "ert"]
+    assert count(xs) == {"tre": 2, "ert": 1}
+
+
+def test_count_random_case_2() -> None:
+    xs: list[str] = ["tre", "tre", "ert", "ert"]
+    assert count(xs) == {"tre": 2, "ert": 2}
